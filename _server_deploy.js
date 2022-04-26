52227b02c9bd4189bad1f83cb8259942a52918eb
neel123go <ayounpaul1029@gmail.com>
2022-04-26 22:25:24 +0600

    add some steps

--- _server_deploy.js
+++ _server_deploy.js
@@ -4,11 +4,12 @@
  * 1. Create Heroku Account
  * 2. Verify Email
  * 3. Install Heroku cli
- * 4. Heroku Login
+ * 4. heroku Login (command)
  * 
  * For each Project one time
  * ---------------------------------------
  * 1. Check Project in Local Server
- * 2. Heroku Create
+ * 2. heroku Create (command)
  * 3. Make sure you: git add. || git commit -m '' || git push
+ * 4. git push heroku main (command)
  */
