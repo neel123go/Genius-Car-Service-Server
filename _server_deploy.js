e1940cda5ff1752349c1b9f90bcf6d8fe2f9c91e
neel123go <ayounpaul1029@gmail.com>
2022-04-26 22:07:31 +0600

    added many things

---
+++ _server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * One Time for your Project
+ * ---------------------------------------
+ * 1. Create Heroku Account
+ * 2. Verify Email
+ * 3. Install Heroku cli
+ * 4. Heroku Login
+ * 
+ * For each Project one time
+ * ---------------------------------------
+ * 1. Check Project in Local Server
+ * 2. Heroku Create
+ * 3. Make sure you: git add. || git commit -m '' || git push
+ */
---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+var uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.bqm81.mongodb.net:27017,cluster0-shard-00-01.bqm81.mongodb.net:27017,cluster0-shard-00-02.bqm81.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xmtu36-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('genius').collection('service');
+        const orderCollection = client.db('genius').collection('order');
+
+        // Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send(accessToken);
+        })
+
+        // Get All Services
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        })
+
+        // Get Single Service
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // Add Service
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        // Delete A Service
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Order Collection API
+
+        // Get Order
+        app.get('/order', verifyToken, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (decodedEmail === email) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            } else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
+        });
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Genius car service connected');
+});
+
+app.listen(port, () => {
+    console.log('Listening to the port', port);
+})
